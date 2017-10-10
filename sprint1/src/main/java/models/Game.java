a5f9e832c9d6b82cfd62913c02e306a222cf937e
Lucas Frey <lcsfrey@users.noreply.github.com>
2017-10-10 16:40:52 -0700

    Added game state methods
    - inEndState() (unfinished) : returns true if no moves can be made
    - hasGameBeenWon() (unfinished) : returns true if inEndState is true and only aces are on the board
    - getScore() : returns the current score

--- sprint1/src/main/java/models/Game.java
+++ sprint1/src/main/java/models/Game.java
@@ -48,9 +48,41 @@ public class Game {
         return this.cols.get(columnNumber).get(this.cols.get(columnNumber).size()-1);
     }
 
-
     public void move(int columnFrom, int columnTo) {
         // remove the top card from the columnFrom column, add it to the columnTo column
+    }
+
+    // returns true if there are no cards left if the deck,
+    // no cards can be moves, and no cards can be removed
+    private boolean inEndState() {
+        if (deck.size() == 0) {
+            // check if cards can be moved from one column to another
+            // check if cards can be removed
+            // if both conditions are false, return true
+        } else {
+            return false;
+        }
+        return false;
+    }
+
+    // returns 1 if the game has been won,
+    //        -1 if the game has been lost,
+    //         0 if moves can still be made.
+    public int hasGameBeenWon() {
+        int cards_left = cols.get(0).size() + cols.get(1).size() + cols.get(2).size() + cols.get(3).size();
+        // The only way cards_left could equal 4 is if the 4 aces are left
+        if (inEndState() && cards_left == 4) {
+            return 1;
+        } else if (inEndState() && cards_left != 4) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    public int getScore() {
+        int cards_left = cols.get(0).size() + cols.get(1).size() + cols.get(2).size() + cols.get(3).size();
+        return 52 - cards_left;
     }
 
     private void addCardToCol(int columnTo, Card cardToMove) {
