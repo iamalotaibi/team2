099a29558ba3c8bd8c5d2e277a417c3e87f5861a
janzeng <janzeng@oregonstate.edu>
2017-10-21 22:30:48 -0700

    More CSS and styling incorporating new elements

---
+++ sprint2/src/main/java/views/AcesUp/AcesUp.js
@@ -0,0 +1,4 @@
+document.body.innerHTML = document.body.innerHTML.replace(/Hearts/g, ' ♥');
+document.body.innerHTML = document.body.innerHTML.replace(/Spades/g, ' ♠');
+document.body.innerHTML = document.body.innerHTML.replace(/Diamonds/g, ' ♦');
+document.body.innerHTML = document.body.innerHTML.replace(/Clubs/g, ' ♣');
