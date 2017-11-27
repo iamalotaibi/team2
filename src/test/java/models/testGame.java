59b92d43435745c55dfac0b17e0895fc4dd15661
AntonSynytsiaOSU <synytsan@oregonstate.edu>
2017-11-26 17:26:43 -0800

    Some work on Game test

--- src/test/java/models/testGame.java
+++ src/test/java/models/testGame.java
@@ -11,37 +11,55 @@ public class testGame {
 
     @Test
     public void testGameCreation(){
-        Game g = new Game();
-        assertNotNull(g);
+        Game g1 = new AcesUpGame();
+        assertNotNull(g1);
+
+        Game g2 = new SpanishGame();
+        assertNotNull(g2);
     }
 
     @Test
     public void testGameStart(){
-        Game g = new Game();
-        g.deck.shuffle();
-        g.dealFour();
-        assertEquals(1, g.cols.get(0).size());
-        assertEquals(1, g.cols.get(1).size());
-        assertEquals(1, g.cols.get(2).size());
-        assertEquals(1, g.cols.get(3).size());
+        Game g1 = new AcesUpGame();
+        g1.deck.shuffle();
+        g1.dealFour();
+        assertEquals(1, g1.cols.get(0).size());
+        assertEquals(1, g1.cols.get(1).size());
+        assertEquals(1, g1.cols.get(2).size());
+        assertEquals(1, g1.cols.get(3).size());
+
+        Game g2 = new SpanishGame();
+        g2.deck.shuffle();
+        g2.dealFour();
+        assertEquals(1, g2.cols.get(0).size());
+        assertEquals(1, g2.cols.get(1).size());
+        assertEquals(1, g2.cols.get(2).size());
+        assertEquals(1, g2.cols.get(3).size());
     }
 
     @Test
     public void testRemoveFunction(){
-        Game g = new Game();
-        ArrayList<Card> list = new ArrayList<Card>();
-        list.add(new Card(0, Suit.Hearts));
-        list.add(new Card(3, Suit.Hearts));
-        list.add(new Card(6, Suit.Hearts));
-        list.add(new Card(9, Suit.Hearts));
-        g.customDeal(list);
+        Game g1 = new AcesUpGame();
+        g1.dealFour();
 
         // test removal of card that can't be removed
-        g.remove(3);
-        assertNotEquals(0, g.cols.get(3).size());
+        g1.remove(3);
+        assertNotEquals(0, g1.cols.get(3).size());
 
         // test removal of card that can be removed
-        g.remove(2);
-        assertEquals(0, g.cols.get(2).size());
+        g1.remove(2);
+        assertEquals(0, g1.cols.get(2).size());
+
+
+        Game g2 = new SpanishGame();
+        g2.dealFour();
+
+        // test removal of card that can't be removed
+        g2.remove(3);
+        assertNotEquals(0, g2.cols.get(3).size());
+
+        // test removal of card that can be removed
+        g2.remove(2);
+        assertEquals(0, g2.cols.get(2).size());
     }
 }
