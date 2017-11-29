d7a90871fb130a3b267b5cb9b653933b10a63abc
janzeng <janzeng@oregonstate.edu>
2017-11-29 01:30:53 -0800

    started htmlString functionality

--- src/main/java/models/Card.java
+++ src/main/java/models/Card.java
@@ -5,9 +5,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.Serializable;
 
+import static models.Suit.*;
+
 public class Card implements Serializable {
     public final int value;
     public final Suit suit;
+    public String htmlString = new String("");
 
     @JsonCreator
     public Card(@JsonProperty("value") int value, @JsonProperty("suit") Suit suit) {
@@ -58,4 +61,86 @@ public class Card implements Serializable {
         }
         return str;
     }
+
+    public String htmlString(Card card) {
+        String str = "";
+        switch(card.suit) {
+        case Hearts:
+            str += "<span class=\"redCard\">\u2665</span>";
+            break;
+        case Spades:
+            str += "<span class=\"blackCard\">\u2660</span>";
+            break;
+        case Diamonds:
+            str += "<span class=\"redCard\">\u2666</span>";
+            break;
+        case Clubs:
+            str += "<span class=\"blackCard\">\u2663</span>";
+            break;
+    }
+
+    str += "<sub class=\"cardID\">";
+    switch(card.value) {
+        case 11:
+            str += "J";
+            break;
+        case 12:
+            str += "Q";
+            break;
+        case 13:
+            str += "K";
+            break;
+        case 14:
+            str += "A";
+            break;
+        default:
+            str += card.value;
+            break;
+    }
+    str += "</sub>";
+
+    return str;
+
+    }
+
+//    function processCard(card) {
+//    var str = "";
+//
+//    switch(card.suit) {
+//        case "Hearts":
+//            str += "<span class=\"redCard\">\u2665</span>";
+//            break;
+//        case "Spades":
+//            str += "<span class=\"blackCard\">\u2660</span>";
+//            break;
+//        case "Diamonds":
+//            str += "<span class=\"redCard\">\u2666</span>";
+//            break;
+//        case "Clubs":
+//            str += "<span class=\"blackCard\">\u2663</span>";
+//            break;
+//    }
+//
+//    str += "<sub class=\"cardID\">";
+//    switch(card.value) {
+//        case 11:
+//            str += "J";
+//            break;
+//        case 12:
+//            str += "Q";
+//            break;
+//        case 13:
+//            str += "K";
+//            break;
+//        case 14:
+//            str += "A";
+//            break;
+//        default:
+//            str += card.value;
+//            break;
+//    }
+//    str += "</sub>";
+//
+//    return str;
+//}
 }
